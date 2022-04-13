38427cd295ac7f2f1dd33989e61a8eee871d970a
001314 <1320123744@qq.com>
2022-04-13 23:28:17 +0800

    third commit

--- src/main/java/com/yida/git.java
+++ src/main/java/com/yida/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.print("hellow git");
         System.out.print("hellow git22");
+        System.out.print("hellow git33");
     }
 }
